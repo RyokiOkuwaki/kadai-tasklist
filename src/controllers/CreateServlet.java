b01994c431fa4f214b97bd994dc4229580a2868d
User <User@DESKTOP-9K86M2L>
2023-05-31 15:51:49 +0900

    バリデーションを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -13,6 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import models.Tasks;
 import utils.DBUtil;
 
+import models.validators.TasksValidator;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+
 /**
  * Servlet implementation class CreateServlet
  */
@@ -46,13 +50,28 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
-            //データベースに保存
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
+            // バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+            List<String> errors = TasksValidator.validate(m);
+            if (errors.size() > 0) {
+                em.close();
 
-            response.sendRedirect(request.getContextPath() + "/index");
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+            } else {
+                // データベースに保存
+                em.persist(m);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+                // indexのページにリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 
